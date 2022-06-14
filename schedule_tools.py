f823976beaa5301c4e3fa3a3193df3d21b90eddf
strarabi <strarabi@gmail.com>
2022-06-14 14:11:20 -0700

    Finish logic for determining valid schedules

--- schedule_tools.py
+++ schedule_tools.py
@@ -4,7 +4,7 @@
 # Ex [[days t1-t2], [days t3-t4]]
 
 def is_valid_schedule(schedule):
-    print(schedule)
+    # These lists represent the schedule of courses for each day
     monday_courses = []
     tuesday_courses = []
     wednesday_courses = []
@@ -14,6 +14,7 @@ def is_valid_schedule(schedule):
     sunday_courses = []
 
     for course in schedule:
+        # Parse the strings and turn them into minute ranges
         split_course = course.split(" ")
         days_of_week = split_course[0]
         time_range = split_course[1].split('-')
@@ -22,6 +23,7 @@ def is_valid_schedule(schedule):
         time_range[1] = time_range[1].replace('a', "").replace('p', "")
         time_range = [time_to_minutes(time_range[0], add), time_to_minutes(time_range[1], add)]
 
+        # Append minute ranges to daily schedules
         for day in days_of_week:
             if day == 'M':
                 monday_courses.append(time_range)
@@ -38,12 +40,26 @@ def is_valid_schedule(schedule):
             elif day == 'U':
                 sunday_courses.append(time_range)
 
+        # Create 2D array where each index is an array of minute ranges for a particular day.
         week_schedule = [monday_courses, tuesday_courses, wednesday_courses, 
         thursday_courses, friday_courses, saturday_courses, sunday_courses]
 
-    print(week_schedule)
+    for day in week_schedule:
+        day.sort()
 
-def time_to_minutes(time, pm): # time e.g 10 or 10:50, pm True or False
+    # In a valid schedule, it should hold that day[i] < day[i+1]
+    # in all cases, since we cannot be in two classes at once.
+    # Thus, if for any day day[i] >= day[i+1], we can remove this combination.
+    for day in week_schedule:
+        for i in range(0, len(day)-1):
+            if day[i] >= day[i+1]:
+                return False
+
+    return week_schedule
+
+# Simple helper function for turning OSU time ranges into minute ranges.
+# Ex. 10-10:50a becomes 600-650.
+def time_to_minutes(time, pm):
     try:
         time = int(time)
     except:
--- server.py
+++ server.py
@@ -9,34 +9,41 @@ app = Flask(__name__)
 
 csv_dict = {}
 
-
+# Read CSV data and create dict
 with open('./data/fa22/fa22_data.csv', mode='r') as inp:
     reader = csv.reader(inp)
     csv_dict = {rows[0]:rows[1] for rows in reader}
 
 curr_classes = []
 
-all_times = []
-
+# Main route for the app, this displays the page where a user
+# can build a schedule.
 @app.route("/")
 @app.route("/main")
 def index():
-    global all_times, curr_classes
-    all_times = []
+    global curr_classes
     curr_classes = []
 
     return render_template("index.html")
 
+# Redirects the user to the about page.
 @app.route("/about")
 def about():
     return render_template("about.html")
 
+# This post method is sent from the browser when
+# a user adds a course, and it appends this course
+# to curr_classes.
 @app.route('/postmethod', methods=['POST'])
 def get_ajax_data():
     js_data = request.form['javascript_data']
     curr_classes.append(js_data)
     return render_template('index.html')
 
+# This post request is fired when the user clicks the 
+# Generate Schedule button. It uses the cartesian product
+# to find all possible schedule combinations, then uses the
+# is_valid_schedule() function to find a valid one.
 @app.route("/generate", methods=['POST'])
 def generate_schedule():
     all_courses_list = []
@@ -45,19 +52,25 @@ def generate_schedule():
     
     all_combinations = list(itertools.product(*all_courses_list))
 
-
+    schedule_generated = False
     for combination in all_combinations:
         if is_valid_schedule(combination):
-            pass
+            print("Found Valid Schedule")
+            for i in range(0, len(combination)):
+                print(curr_classes[i], " at ", combination[i])
+                schedule_generated = True
+            break
         else:
             pass
+    if not schedule_generated:
+        print("Could not generate a schedule from the given courses.")
 
     return render_template('index.html')
 
+# Clears the curr_classes list.
 @app.route("/clear", methods=['POST'])
 def clear_schedule():
-    global all_times, curr_classes
-    all_times = []
+    global curr_classes
     curr_classes = []
     return render_template('index.html')
 
