4fc4128c0c31719ea435e12581aed71b38f396fc
strarabi <strarabi@gmail.com>
2022-06-14 20:17:26 -0700

    Use sessions instead of globals, change image size

--- server.py
+++ server.py
@@ -1,11 +1,15 @@
 import csv
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
+from flask_session import Session
 from schedule_tools import is_valid_schedule
 import itertools
 import numpy as np
 import json
 
 app = Flask(__name__)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 csv_dict = {}
 
@@ -14,16 +18,12 @@ with open('./data/fa22/fa22_data.csv', mode='r') as inp:
     reader = csv.reader(inp)
     csv_dict = {rows[0]:rows[1] for rows in reader}
 
-curr_classes = []
-
 # Main route for the app, this displays the page where a user
 # can build a schedule.
 @app.route("/")
 @app.route("/main")
 def index():
-    global curr_classes
-    curr_classes = []
-
+    session['curr_classes'] = []
     return render_template("index.html")
 
 # Redirects the user to the about page.
@@ -33,11 +33,11 @@ def about():
 
 # This post method is sent from the browser when
 # a user adds a course, and it appends this course
-# to curr_classes.
+# to the session's current classes.
 @app.route('/addcourse', methods=['POST'])
 def get_ajax_data():
     course = request.form['course']
-    curr_classes.append(course)
+    session['curr_classes'].append(course)
     return render_template('index.html')
 
 # This post request is fired when the user clicks the 
@@ -47,7 +47,7 @@ def get_ajax_data():
 @app.route("/generate", methods=['GET'])
 def generate_schedule():
     all_courses_list = []
-    for course in curr_classes:
+    for course in session['curr_classes']:
         all_courses_list.append((csv_dict[course.rstrip()]).replace("'", "").strip('][').split(', '))
     
     all_combinations = list(itertools.product(*all_courses_list))
@@ -56,7 +56,7 @@ def generate_schedule():
         if is_valid_schedule(combination):
             schedule_str = ""
             for i in range(0, len(combination)):
-                schedule_str += (curr_classes[i] + " at " + combination[i] + '\n')
+                schedule_str += (session['curr_classes'][i] + " at " + combination[i] + '\n')
             print(schedule_str.rstrip())
             return schedule_str.rstrip()
     return "Could not generate a schedule from the selected courses."
@@ -64,8 +64,7 @@ def generate_schedule():
 # Clears the curr_classes list.
 @app.route("/clear", methods=['POST'])
 def clear_schedule():
-    global curr_classes
-    curr_classes = []
+    session['curr_classes'] = []
     return render_template('index.html')
 
 if __name__ == "__main__":
