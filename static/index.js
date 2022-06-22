a763574f87825b6da98d3243493c3603a3b1f667
strarabi <strarabi@gmail.com>
2022-06-21 18:44:58 -0700

    Don't allow inputs that aren't in the datalist

--- static/index.js
+++ static/index.js
@@ -13,22 +13,28 @@ function create_courses_datalist(courses_array) {
 window.onload = function() {
     // When the user adds a new class, send it to the backend
     document.querySelector('input').onchange = function() {
-        $.post("/addcourse", {
-            course: this.value
-        })
-        // Display the new class that the user has added on the page
-        var new_div = document.createElement('div')
-        new_div.classList.add('classes-info')
-        new_div.textContent = (" " + this.value)
-        new_div.addEventListener('click', function() {
-            $.post("/removecourse", {
-                course: this.textContent
-            })
-            this.remove()
-        })
-        var div_container = document.getElementsByClassName('classes-container')[0]
-        div_container.appendChild(new_div)
+        var datalist = document.getElementById('courses').childNodes
+        for (var i = 0; i < datalist.length; i++) {
+            if (datalist[i].value == this.value) {
+                $.post("/addcourse", {
+                    course: this.value
+                })
+                // Display the new class that the user has added on the page
+                var new_div = document.createElement('div')
+                new_div.classList.add('classes-info')
+                new_div.textContent = (" " + this.value)
+                new_div.addEventListener('click', function() {
+                    $.post("/removecourse", {
+                        course: this.textContent
+                    })
+                    this.remove()
+                })
+                var div_container = document.getElementsByClassName('classes-container')[0]
+                div_container.appendChild(new_div)
+            }
+        }
     }
+
     // Attach event listeners to generate and clear buttons,
     // making appropriate requests to the backend.
     generate_button = document.getElementById('generate-button')
@@ -55,6 +61,7 @@ window.onload = function() {
 
     select_term_dropdown = document.getElementById('term-selection')
     select_term_dropdown.addEventListener('change', function() {
+        $.post("/clear")
         $.ajaxSetup({async: false})
         var courses_array = $.post("/offerings", {
             term: this.value
